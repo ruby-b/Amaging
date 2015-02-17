bac93a4067aaf8fae1dfbf179e2613d4706d9d11
h-matsumoto <h-matsumoto@ankh-systems.co.jp>
2015-02-17 14:43:21 +0900

    seedを追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Movie.create(:name => '大日本人', :detail => '松本人志監督第1回作品として超注目を集めたこの作品。', :release_date => '2014-10-30', :rate => 1, :img_path => "http://flowerwild.net/images/dainihonjin001.jpg")
+Movie.create(:name => '大日本人', :detail => '松本人志監督第1回作品として超注目を集めたこの作品。', :release_date => '2014-10-30', :status => 1, :img_path => "http://flowerwild.net/images/dainihonjin001.jpg")
+Movie.create(:name => 'しんぼる', :detail => '想像もつかない“何か”が起こる…', :release_date => '2014-11-15', :status => 1, :img_path => "http://www.popandpop1980.com/2009082100101.jpg")
+Movie.create(:name => 'さや侍', :detail => 'さや侍","鞘しか持たない侍とその娘、30日間の戦い―。', :release_date => '2014-12-8', :status => 1, :img_path => "http://img.cinematoday.jp/a/N0032866/_size_240x/_v_1307384215/main.jpg")
