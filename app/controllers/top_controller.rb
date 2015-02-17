4d793186e4a8db2cc5ee9f609698cbe746c08e43
h-matsumoto <h-matsumoto@ankh-systems.co.jp>
2015-02-17 16:16:43 +0900

    ログイン処理を修正

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,10 +1,16 @@
 class TopController < ApplicationController
-#  before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
 
   def show
     #映画テーブルからデータを取得
     @movie = Movie.all
+
+    p "***********************************"
+    p current_user.id
+    p "***********************************"
+
+
   end
 
 end
