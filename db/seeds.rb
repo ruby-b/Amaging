b2460e276691653f73723010643f49d6bbf848e6
h-matsumoto <h-matsumoto@ankh-systems.co.jp>
2015-02-17 13:29:33 +0900

    seedをコミット

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -5,3 +7,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Movie.create(:name => '大日本人', :detail => '松本人志監督第1回作品として超注目を集めたこの作品。', :release_date => '2014-10-30', :status => 1, :img_path => "http://flowerwild.net/images/dainihonjin001.jpg")
