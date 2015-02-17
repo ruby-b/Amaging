58c8533c873575801b9357a48a4c583d2b5c8272
h-matsumoto <h-matsumoto@ankh-systems.co.jp>
2015-02-17 17:36:33 +0900

    デバックコードを削除

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,16 +1,13 @@
 class TopController < ApplicationController
-#  before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
 
   def show
     #映画テーブルからデータを取得
     @movie = Movie.all
 
-    p "***********************************"
-    p current_user.id
-    p "***********************************"
 
-
+    
   end
 
 end
