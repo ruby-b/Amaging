5db356c8ed243a156093903c3138c26098091899
t-niina <t-niina@allmedia.co.jp>
2015-02-17 16:57:05 +0900

    スターレーティング修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,3 +6,7 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( jquery-star-rating.css )
+Rails.application.config.assets.precompile += %w( jquery.rating.css )
+Rails.application.config.assets.precompile += %w( star.gif )
+Rails.application.config.assets.precompile += %w( delete.gif )
